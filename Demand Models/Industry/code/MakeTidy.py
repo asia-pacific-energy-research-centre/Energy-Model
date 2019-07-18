3312a4a1eae970b563868e0281d732c7b28fda96
David Wogan <wogandavid@users.noreply.github.com>
2019-07-18 11:48:19 +0900

    Created merged dataset

--- Demand Models/Industry/code/MakeTidy.py
+++ Demand Models/Industry/code/MakeTidy.py
@@ -10,8 +10,8 @@ RawSteelData = pd.read_csv(r'data\raw\IS_production7th.csv')
 RawSteelData.head()
 
 # using melt to make a tidy set with multiple measured variables - see Table 12 in Tidy Data paper
-TidySteel = (pd.melt(RawSteelData, id_vars=['Economy'], var_name='Year')
-                  .set_index(['Economy','Year']))
+TidySteel = pd.melt(RawSteelData, id_vars=['Economy'], var_name='Year')
+TidySteel.rename(columns={'value':'SteelProduction'}, inplace=True)
 
 # write to csv
 TidySteel.to_csv(r'data\modified\TidySteel.csv', index=False)
---
+++ Demand Models/Industry/code/createdatasets.py
@@ -0,0 +1,16 @@
+# createdatasets.py
+# join together GDP, population, and production
+
+# import math and data table functions
+import numpy as np
+import pandas as pd
+
+# read data from csv and store as dataframe
+TidySteel = pd.read_csv(r'data\modified\TidySteel.csv')
+GDP7thHistorical = pd.read_csv(r'C:\GitHub\Energy-Model\SharedData\GDP7thHistorical.csv')
+Pop7thHistorical = pd.read_csv(r'C:\GitHub\Energy-Model\SharedData\Pop7thHistorical.csv')
+
+# combine datasets
+SteelHistorical = pd.merge(GDP7thHistorical, TidySteel, how='left', on=['Economy','Year'])
+SteelHistorical = pd.merge(SteelHistorical,Pop7thHistorical,how='left',on=['Economy','Year'])
+
