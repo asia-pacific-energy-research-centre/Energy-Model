1560da68e873ba1e748bcff8bee8f50c7e99bd49
David Wogan <wogandavid@users.noreply.github.com>
2019-07-23 10:37:43 +0900

    Add raw and modified data
    Also cleaned up MakeEGEDATidy.py

--- EGEDA/code/MakeEGEDATidy.py
+++ EGEDA/code/MakeEGEDATidy.py
@@ -2,11 +2,14 @@
 # Take EGEDA 2016 data and reshape in Tidy format
 # https://stackoverflow.com/questions/45066873/pandas-melt-with-multiple-value-vars
 
+# raw data file = APEC21_22Jul2019B_raw.xlsx
 # manual adjustments made to the raw data file before the following code:
-# - removed summary sheets
 # - renamed 'MYS' to 'MAS'
 # - removed column labels for columns A,B,C,D
 # - removed summary rows at bottom of each sheet
+# - removed miscellaneous calculations in row AP
+
+# to use file, open code editor to the EGEDA folder (so file paths will be correct)
 
 #### Begin data manipulation ####
 
@@ -14,18 +17,12 @@
 import numpy as np
 import pandas as pd
 
-# read in EGEDA file in to a dictionary. All sheets are read in.
-# note Canada is missing years 1980-2003. Filled in manually to 0.
-RawEGEDA = pd.read_excel(r'data\modified\APEC21_22Jul2019B_basic.xlsx', sheet_name=None)
+# read all sheets from EGEDA spreadsheet to Pandas dictionary
+# Pandas dictionary contains a key for each economy, and the value is a dataframe with the table data
+RawEGEDA = pd.read_excel(r'data\modified\APEC21_22Jul2019B_ready.xlsx', sheet_name=None)
 
 # define year range
 years = list(range(1980,2016,1))
-
-# rename economies
-economy_names = {'AUS':'01_AUS','BRN':'02_BD','CAN':'03_CDA','CHL':'04_CHL','CHN':'05_PRC',
-        'HKG':'06_HKC','IDN':'07_INA','JPN':'08_JPN','KOR':'09_KOR','MAS':'10_MAS', 'MEX':'11_MEX',
-        'NZL':'12_NZ','PNG':'13_PNG','PER':'14_PE','PHL':'15_RP','RUS':'16_RUS','SGP':'17_SIN',
-        'CT':'18_CT','THA':'19_THA','USA':'20_USA','VNM':'21_VN'}
 
 # create empty list to store each dataframe
 df_list =[]
@@ -41,15 +38,31 @@ for sheet, dataframe in RawEGEDA.items():
         df_name['Economy'] = sheet
         df_list.append(df_name)
 
-# combine individual economy dataframes in to one
+# combine individual economy dataframes to one dataframe
 dfResults = pd.concat(df_list).reset_index(drop=True)
 
 # remove x and X placeholders with NaNs
-dfResults[dfResults.Energy == 'x'] = np.NaN
-dfResults[dfResults.Energy == 'X'] = np.NaN
+# code..
+# code..
 
 # replace economy names
-dfResults.replace(economy_names, inplace=True)
+# rename economies
+economy_names = {'AUS':'01_AUS','BRN':'02_BD','CAN':'03_CDA','CHL':'04_CHL','CHN':'05_PRC',
+        'HKG':'06_HKC','IDN':'07_INA','JPN':'08_JPN','KOR':'09_KOR','MAS':'10_MAS', 'MEX':'11_MEX',
+        'NZL':'12_NZ','PNG':'13_PNG','PER':'14_PE','PHL':'15_RP','RUS':'16_RUS','SGP':'17_SIN',
+        'CT':'18_CT','THA':'19_THA','USA':'20_USA','VNM':'21_VN'}
+# code to replace..
+
+# add column to store APERC codes
+dfResults['APERC'] = np.NaN
+
+# create dictionary of Product Code and APERC code
+#APERC_codes = {
+#        '02. Coal Products':'CoalP',
+#        '03. Crude Oil & NGL':'OilP'
+#        }
+
+#dfResults.replace(APERC_codes, inplace=True)
 
 # write to csv
 dfResults.to_csv(r'data\modified\TidyEGEDA.csv', index=False)
