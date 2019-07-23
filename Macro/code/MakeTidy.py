0b1a17ca3f73c373e51aac6a201341a143daba10
cdoleman <51557209+cdoleman@users.noreply.github.com>
2019-07-23 10:26:10 +0900

    Dropped NA's from Macro data (#6)

--- Macro/code/MakeTidy.py
+++ Macro/code/MakeTidy.py
@@ -11,7 +11,7 @@ rawPop7th = pd.read_excel(r'data\raw\Population_7th_2019_07_02 - raw.xlsx')
 rawGDP7th = pd.read_excel(r'data\raw\GDP_7th_2019_07_02 - raw.xlsx')
 
 # define list of years
-year_list = list(range(1990,2050,1))
+year_list = list(range(1990,2051,1))
 
 # use melt to collapse the year columns in to a new column and rename columns
 Pop7th = pd.melt(rawPop7th, id_vars='Economy', value_vars=year_list)
@@ -19,6 +19,11 @@ Pop7th.rename(columns={'variable':'Year','value':'Population'}, inplace=True)
 
 GDP7th = pd.melt(rawGDP7th, id_vars='Economy', value_vars=year_list)
 GDP7th.rename(columns={'variable':'Year','value':'GDP'}, inplace=True)
+
+# remove World Economy data
+
+Pop7th.dropna(inplace=True)
+GDP7th.dropna(inplace=True)
 
 # separate historical and future
 # store all values up up to, and including, 2016 in one dataframe
